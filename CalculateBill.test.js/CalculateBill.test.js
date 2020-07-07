005585cc9c61fe47e65713e7ee5d3fa779cfc8c2
yongama <yongamafayo@gmail.com>
2020-07-07 13:12:30 +0200

    first commit

---
+++ CalculateBill.test.js/CalculateBill.test.js
@@ -0,0 +1,20 @@
+describe("CallTotal", function () {
+
+    it("should be able to set total amount", function () {
+        let settingsTest = CalculateBill();
+
+        // settingsTest.setCallTotal("call");
+        // assert.equal(2.75, settingsTest.getCallTotal());
+
+    });
+
+    // it("total must show colour code for warning and danger level", function () {
+    //     let settingsTest = CalculateBill();
+
+    //     // settingsTest.setCallTotal("call");
+    //     // assert.equal(2.75, settingsTest.getCallTotal());
+
+    // });
+
+
+});     
---
+++ CalculateFactoryFunction.js/calculateFactoryFunction.js
@@ -0,0 +1,23 @@
+function CalculateBill(billItem) {
+
+
+    var billItems = billItem.split(",");
+
+    var billTotal = 0;
+
+    for (var i = 0; i < billItems.length; i++) {
+        var billItem = billItems[i].trim();
+
+        if (billItem === "call") {
+            billTotal += 2.75;
+        }
+
+        else if (billItem === "sms") {
+            billTotal += 0.75;
+        }
+
+    }
+
+    return billTotal
+}
+
